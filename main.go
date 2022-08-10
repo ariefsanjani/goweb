36b3bec542c4312f433892945d545a9f776bb95a
ariefsanjani <sanjani.punk@gmail.com>
2022-08-11 03:33:02 +0700

    make it better

--- main.go
+++ main.go
@@ -15,8 +15,8 @@ func main() {
 	fileServer := http.FileServer(http.Dir("assets"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Println("Starting web on port")
+	log.Println("Starting web on port 8080")
 
-	err := http.ListenAndServe(":", mux)
+	err := http.ListenAndServe(":8080", mux)
 	log.Fatal(err)
 }
